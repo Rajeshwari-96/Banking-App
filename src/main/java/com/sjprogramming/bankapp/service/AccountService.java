aa8eabc956ba6351db5f3f02f05c75409a9f8b6c
Rajeshwari Nandikanti <rajarajeshwari852@gmail.com>
2024-07-16 19:41:13 -0400

    intial commit for banking application

---
+++ src/main/java/com/sjprogramming/bankapp/service/AccountService.java
@@ -0,0 +1,15 @@
+package com.sjprogramming.bankapp.service;
+import java.util.List;
+import com.sjprogramming.bankapp.entity.Account;
+
+public interface AccountService {
+
+	public Account createAccount(Account account);
+	public Account getAccountDetailsByAccountNumber(Long accccountNumber);
+	public  List <Account> getAllAccountDetails();
+	public Account depositAmount(Long accountNumber, Double amount);
+	public Account withdrawAmount(Long accountNumber, Double amount);
+	public void closeAccount(Long accountNumber);
+	
+
+}
